784e7626b81a974ca18c4f5594ba7d2aaf037647
utan323 <nglephuc03@gmail.com>
2024-03-25 10:16:19 +0700

    feat: add icons on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-import {RiListCheck} from "react-icons/ri";
+import {FaBars} from "react-icons/fa6";
+import { IoIosSearch } from "react-icons/io";
+import { PiSuitcaseSimple } from "react-icons/pi";
 import { useState, useEffect } from "react";
 
 import Menu from "./Menu";
@@ -7,7 +9,6 @@ const Navbar = () => {
     const [isMenuOpen, setMenuStatus] = useState(false);
 
     const handleMenu = () => {
-        console.log(isMenuOpen);
         setMenuStatus(!isMenuOpen);
     }
 
@@ -17,16 +18,26 @@ const Navbar = () => {
                 <div className="basis-1/2 my-auto">
                     <div className="ml-5">
                         <div style={{display: (isMenuOpen ? "none" : "block")}}>
-                           <RiListCheck size={"3rem"} onClick={() => {handleMenu()}}/> 
+                           <FaBars size={"3rem"} onClick={() => {handleMenu()}}/> 
                         </div>
                         <div className={`flex absolute inset-0 bg-white ${isMenuOpen ? 'w-[25%] h-[100%]' : 'w-[0%] h-[100%]' } duration-[0.25s] transition-all ease-in-out`}  style={{}}>
                            <div className={`flex-1 ${isMenuOpen ? "block" : "hidden"}`}><Menu handleMenu={handleMenu}/> </div>
                         </div>
                     </div>
                 </div>
-                <div className="basis-1/2">Logo</div>
+                <div className="basis-1/2">
+                    <a href="#">
+                        Logo
+                    </a>
+                </div>
             </div>
-            <div className="basis-1/2">tool bar</div>
+            <div className="flex flex-1 justify-end">
+                <input className="h-1/2 w-1/3 mx-2 my-auto rounded"/>
+                <IoIosSearch className="my-auto" size={"2.5rem"}/>
+                <PiSuitcaseSimple className="ml-8 mr-3 my-auto" size={"2.5rem"}/>
+                <a className="h-[90%] w-10 mx-3 my-auto flex">
+                </a>
+            </div>
         </div>
     )
 };
