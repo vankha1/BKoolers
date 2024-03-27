d55c84d6a7bba6506df7b822b85378199201258a
Fooc <nglephuc03@gmail.com>
2024-03-27 20:22:11 +0700

    refactor navbar

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -3,15 +3,15 @@ import useCheckDeviceScreen from "../customizes/useCheckDeviceScreen";
 import { useState, useEffect } from "react";
 
 const NotifyIcon = (props) => {
-    const {numb} = props;
-    
+    const { numb } = props;
+
     if (numb == 0) return null;
     else if (numb > 99)
         return (
             <span className="absolute -right-1 h-5 w-5 rounded-full bg-red-600 flex justify-center items-center items">
                 <span className="text-white text-xs">99+</span>
             </span>
-        );  
+        );
     else return (
         <span className="absolute -right-1 h-5 w-5 rounded-full bg-red-600 flex justify-center items-center items">
             <span className="text-white text-xs">{numb}</span>
@@ -23,14 +23,14 @@ const Cart = (props) => {
     const [itemNums, setItemNums] = useState(3);
 
     const isMobile = useCheckDeviceScreen();
-    console.log(itemNums); 
+    console.log(itemNums);
     return (
-        <> 
-            <NotifyIcon numb={itemNums}/>
+        <>
+            <NotifyIcon numb={itemNums} />
             <div>
-                <PiSuitcaseSimple size={"3rem"}/>
+                <PiSuitcaseSimple size={25} />
             </div>
-        </>    
+        </>
     )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {FaBars} from "react-icons/fa6";
+import { FaBars } from "react-icons/fa6";
 import { IoIosSearch } from "react-icons/io";
 import { useState, useEffect } from "react";
 
@@ -13,16 +13,16 @@ const Navbar = () => {
     }
 
     return (
-        <div className="h-[8%] flex justify-between align-middle bg-gray-600">
-            <div className="flex basis-1/2 justify-between">
+        <div className="px-5 h-[8%] flex justify-between align-middle bg-gray-600">
+            <div className="flex basis-3/5 justify-between">
                 <div className="basis-1/2 my-auto">
                     <div>
-                        <div className={`ml-5 ${isMenuOpen ? "hidden" : "block"}`}>
-                           <FaBars size={"3rem"} onClick={() => {handleMenu()}}/> 
+                        <div className={`h-fit ${isMenuOpen ? "hidden" : "block"}`}>
+                            <FaBars size={25} onClick={() => { handleMenu() }} />
                         </div>
-                        <div className={`absolute inset-0 z-[99] w-screen bg-black/20 ${isMenuOpen ? "h-screen" : "h-0"}`}>
-                            <div className={`flex h-full bg-white ${isMenuOpen ? 'w-[25%]' : 'w-[0%]' } duration-[0.25s] transition-all ease-in-out`}>
-                               <Menu handleMenu={handleMenu} menuState={isMenuOpen}/>
+                        <div className={`absolute inset-0 z-[99] h-screen bg-black/20 ${isMenuOpen ? "w-screen" : "w-0"} duration-[0.2s]`}>
+                            <div className={`flex h-full bg-white ${isMenuOpen ? 'w-1/2 lg:w-[25%]' : 'w-[0%]'} duration-[0.25s] transition-width ease-in-out`}>
+                                <Menu handleMenu={handleMenu} menuState={isMenuOpen} />
                             </div>
                         </div>
                     </div>
@@ -33,11 +33,11 @@ const Navbar = () => {
                     </a>
                 </div>
             </div>
-            <div className="flex flex-1 justify-end">
-                <input className="h-1/2 w-1/3 mx-2 my-auto rounded"/>
-                <IoIosSearch className="my-auto" size={"2.5rem"}/>
+            <div className="flex basis-2/5 justify-end">
+                <input className="h-1/2 w-1/3 mx-2 my-auto rounded" />
+                <IoIosSearch className="my-auto" size={25} />
                 <div className="relative inline-flex ml-8 mr-3 my-auto">
-                    <Cart/>
+                    <Cart />
                 </div>
                 <a className="h-[90%] w-10 mx-3 my-auto flex">
                 </a>
